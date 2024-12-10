cd8fd7891aeeafe55ee06c11e65fe730ef43f080
Tung Le <lenhattung@gmail.com>
2024-12-10 13:40:10 +0700

    update
    update

--- src/nhom_31/flutter_02/lib/examples.dart
+++ src/nhom_31/flutter_02/lib/examples.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'baitap01.dart';
 
 class CardExample extends StatelessWidget {
   @override
@@ -72,6 +73,68 @@ class BorderExample extends StatelessWidget {
               child: Text('Border Example'),
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+///
+
+class BottomNavigationBarExample extends StatefulWidget {
+  const BottomNavigationBarExample({super.key});
+
+  @override
+  State<BottomNavigationBarExample> createState() => _BottomNavigationBarExampleState();
+}
+
+//
+class _BottomNavigationBarExampleState extends State<BottomNavigationBarExample> {
+  int _selectedIndex = 0;
+  // Sử dụng các class có sẵn
+  static final List<Widget> _widgetOptions = <Widget>[
+    const BorderExample(),
+    const CircleAvatarExample(),
+    const ProfilePage(),
+  ];
+  // Hàm xử lý khi tap vào item
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('BottomNavigationBar'),
+        ),
+        body: _widgetOptions.elementAt(_selectedIndex),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            )
+          ],
+          // Index cua item dang duoc chon
+          currentIndex: _selectedIndex,
+          // Màu của item khi được chọn
+          selectedItemColor: Colors.amber[800],
+          // Hàm xử lý khi tap vào item
+          onTap: _onItemTapped,
         ),
       ),
     );
