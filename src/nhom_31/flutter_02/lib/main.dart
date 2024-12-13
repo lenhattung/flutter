bccb1610b5fa2c750b81948adbe4dcb085de39f7
Tung Le <lenhattung@gmail.com>
2024-12-13 08:06:24 +0700

    update
    update

--- src/nhom_31/flutter_02/lib/main.dart
+++ src/nhom_31/flutter_02/lib/main.dart
@@ -2,24 +2,24 @@ import 'package:flutter/material.dart';
 import 'examples.dart';
 import 'baitap01.dart';
 import 'baitap02.dart';
+import 'baitap03.dart';
 
-void main() {
+void main(){
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Examples',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primaryColor: Colors.blue,
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: BaiTap3(),
     );
   }
 }
