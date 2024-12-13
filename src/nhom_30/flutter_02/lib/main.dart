4e87bf1e623d6a90c4463b3a5ce0f579fe659aa3
Tung Le <lenhattung@gmail.com>
2024-12-13 09:46:32 +0700

    update
    update

--- src/nhom_30/flutter_02/lib/main.dart
+++ src/nhom_30/flutter_02/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'examples.dart';
+import 'baitap01.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,8 +18,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const CardExample(),
+      home: const GridPage3(),
     );
   }
 }
+
 
