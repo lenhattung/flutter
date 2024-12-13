9b2c720d444498fffc2ee2b4a7710b6e1523db6d
Tung Le <lenhattung@gmail.com>
2024-12-13 08:24:18 +0700

    update
    update

--- src/nhom_30/flutter_02/lib/examples.dart
+++ src/nhom_30/flutter_02/lib/examples.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'baitap01.dart';
 
 class CardExample extends StatelessWidget {
+  const CardExample({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
