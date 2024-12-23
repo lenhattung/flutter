5961f61eaff57738ae823a42ae98deecb6eb369c
Tung Le <lenhattung@gmail.com>
2024-12-23 14:25:53 +0700

    update
    update

--- src/nhom_24/flutter_05/lib/main.dart
+++ src/nhom_24/flutter_05/lib/main.dart
@@ -1,9 +1,28 @@
+// Bước 1: Thêm dependency vào pubspec.yaml
+// dependencies:
+//   provider: ^6.1.1
+//   flutter:
+//     sdk: flutter
+
+// Bước 2: Chạy lệnh trong terminal
+// flutter pub add provider
+// hoặc
+// flutter pub get
+
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 //import 'bai_1/ex1.dart';
-import 'bai_2/ex2.dart';
+//import 'bai_2/ex2.dart';
+import 'bai_3/ex3.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      ChangeNotifierProvider(
+        create: (context)=>CounterProvider(),
+        child: const MyApp(),
+      ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,7 +37,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: CounterScreen(),
+      home: const CounterScreen(),
     );
   }
 }
