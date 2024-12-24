393567b3b74f6b895342671ff5f0fd5c0edfd306
Tung Le <lenhattung@gmail.com>
2024-12-24 13:02:12 +0700

    update
    update

---
+++ src/nhom_31/flutter_05/lib/bai_1/ex1.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class UserInfoWiget extends StatelessWidget{
+  final String username;
+  final String email;
+
+  const UserInfoWiget({
+    super.key,
+    required this.username,
+    required this.email,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Username: $username',
+            style: const TextStyle(fontSize: 20),
+        ),
+        Text('Email: $email',
+          style: const TextStyle(fontSize: 16),
+        ),
+      ],
+    );
+  }
+}
+//=============
+class UserProfileScreen extends StatelessWidget{
+  const UserProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: const Text("Thông tin người dùng"),
+     ),
+     body: const Center(
+       child: UserInfoWiget(username: "tungit07", email: "lenhattung@gmail.com"),
+     ),
+   );
+  }
+}
